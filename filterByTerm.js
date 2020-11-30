c0b81cf92dd8b613a4e3c6627d9cf1f0f2f6174f
bhojrajjoshi2020 <60560151+bhojrajjoshi2020@users.noreply.github.com>
2020-11-30 14:22:47 +0530

    feat: added new file

---
+++ filterByTerm.js
@@ -0,0 +1,26 @@
+function filterByTerm(inputArr, searchTerm) {
+  if (!searchTerm) throw Error("searchTerm cannot be empty");
+  if (!inputArr.length) throw Error("inputArr cannot be empty"); // new line
+  const regex = new RegExp(searchTerm, "i");
+  return inputArr.filter(function(arrayElement) {
+    return arrayElement.url.match(regex);
+  });
+}
+
+
+const codeSmell = () => {
+    let a = 5;
+  return a;
+    a=398234;
+}
+
+
+
+module.exports = {
+    filterByTerm,
+    codeSmell
+}
+
+
+
+
